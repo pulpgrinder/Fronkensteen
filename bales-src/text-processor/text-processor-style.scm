313b4ab89395d3144ee85d4443ea1391381b4b88
Tony Hursh <pulpgrinder@protonmail.com>
2020-04-10 07:19:02 -0800

    Text processor UI.

---
+++ bales-src/text-processor/text-processor-style.scm
@@ -0,0 +1,9 @@
+(install-css "text-processor-style"
+(proc-css-list `(
+    (".fronkensteen-footnote-link" (
+      "font-size" "75%"
+      "vertical-align" "super"
+      "line-height" "0"
+      ))
+
+      )))
