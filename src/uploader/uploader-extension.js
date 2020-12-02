a241cca977de2398feeefb54f9aa50feb4fec104
Pulpgrinder <pulpgrinder@protonmail.com>
2020-12-01 18:58:23 -0900

    Uploader and downloader tweaks.

--- src/uploader/uploader-extension.js
+++ src/uploader/uploader-extension.js
@@ -3,18 +3,9 @@
 // Copyright 2020 by Anthony W. Hursh
 // MIT License.
 
-Fronkensteen.uploadElement = null;
-Fronkensteen.setUploadElement = function(element_id){
-  Fronkensteen.uploadElement = element_id;
-  return $("#"+ Fronkensteen.uploadElement)
-}
 
 Fronkensteen.uploadFile = function(type,multiple,proc){
-  if(Fronkensteen.uploadElement === null){
-    console.error("file uploader not defined. Try running (build-uploader) first.");
-    return false;
-  }
-  let uploader = $("#"+ Fronkensteen.uploadElement);
+  let uploader = $("#fronkensteen-upload-element");
   if(type !== false){
     uploader.attr('accept', type);
   }
@@ -31,7 +22,7 @@ Fronkensteen.uploadFile = function(type,multiple,proc){
   uploader.off("change");
   uploader.val('');
   uploader.change(function(){
-    let curFiles = $("#" + Fronkensteen.uploadElement)[0].files;
+    let curFiles = uploader[0].files;
     for (var i = 0; i < curFiles.length; i++) {
       let reader = new FileReader();
       reader.onloadend = (function(filename) {
@@ -58,7 +49,6 @@ Fronkensteen.downloadFile = function(filename,data,mime_type){
   element.innerHTML = "Download";
   //document.body.appendChild(element)
   setTimeout(function(){
-    console.log("Clicking")
     element.click();
   },1500);
 
@@ -87,7 +77,7 @@ Fronkensteen.downloadInternalFile = function(filename){
 
 BiwaScheme.define_libfunc("upload-file", 3, 3, function(ar, intp){
   let result = Fronkensteen.uploadFile(ar[0],ar[1],ar[2]);
-  setTimeout(function(){$("#" + Fronkensteen.uploadElement + "-clickupload").click()},20);
+  setTimeout(function(){$("#fronkensteen-upload-element").click()},20);
   return result;
 });
 
@@ -101,8 +91,4 @@ BiwaScheme.define_libfunc("download-file", 3, 3, function(ar, intp){
   BiwaScheme.assert_string(ar[1]);
   BiwaScheme.assert_string(ar[2]);
   Fronkensteen.downloadFile(ar[0],ar[1],ar[2]);
-});
-
-BiwaScheme.define_libfunc("set-upload-element", 1, 1, function(ar, intp){
-  return Fronkensteen.setUploadElement(ar[0]);
 });
