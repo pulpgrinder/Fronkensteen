2a9c4ad723b6e4ca67bca1a0694d9b9953b72457
Pulpgrinder <pulpgrinder@protonmail.com>
2020-12-13 04:36:56 -0900

    Added rules for embedded Scheme and JS to Fronkenmark editor mode.

--- src/13-code-editor/fronkenmark-mode.js
+++ src/13-code-editor/fronkenmark-mode.js
@@ -4,10 +4,13 @@ CodeMirror.defineSimpleMode("fronkenmark", {
   start: [
     // The regex matches the token, the token property contains the type
     {regex: /^\;.*$/, token: "comment"},
+    {regex: /\[!scheme/m, token: "type", mode: {spec: "scheme", end: /scheme!\]/m}},
+    {regex: /\[!javascript/m, token: "type", mode: {spec: "javascript", end: /javascript!\]/m}},
     {regex: /\[[!a-zA-Z][a-zA-Z0-9]*(\s|$)/m, token: "type"},
     {regex: /(\s|^)[a-zA-Z][a-zA-Z0-9!]*\]/m, token: "type"},
     {regex: /\[nosmart\]/m, token: "type"},
     {regex: /\[hr\]/m, token: "type"},
     {regex: /\[br\]/m, token: "type"}
+
   ]
   } );
