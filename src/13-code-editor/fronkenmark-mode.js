ac05e755904cfcf8edc7e08f09a2784f3fce0d6d
Pulpgrinder <pulpgrinder@protonmail.com>
2020-12-13 20:13:47 -0900

    Improving syntax highlighting in code editor.

--- src/13-code-editor/fronkenmark-mode.js
+++ src/13-code-editor/fronkenmark-mode.js
@@ -3,7 +3,7 @@ CodeMirror.defineSimpleMode("fronkenmark", {
   // The start state contains the rules that are intially used
   start: [
     // The regex matches the token, the token property contains the type
-    {regex: /^\;.*$/, token: "comment"},
+    {regex: /\s*\;.*$/, token: "comment",sol:true},
     {regex: /\[!scheme/m, token: "type", mode: {spec: "scheme", end: /scheme!\]/m}},
     {regex: /\[!javascript/m, token: "type", mode: {spec: "javascript", end: /javascript!\]/m}},
     {regex: /\[[!a-zA-Z][a-zA-Z0-9]*(\s|$)/m, token: "type"},
