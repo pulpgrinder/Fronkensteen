e021875bfa75c8eef60f084788f1d021f5292e63
Pulpgrinder <pulpgrinder@protonmail.com>
2020-12-22 16:11:40 -0900

    Cleaning up interaction between editor and file system.

--- src/13-code-editor/generic-editor-fs.scm
+++ src/13-code-editor/generic-editor-fs.scm
@@ -5,17 +5,17 @@
       "(new file)")))
 
 (define (generic-editor-file-writer id)
-  (let ((old-path (get-generic-editor-resource-path id)))
-      (let ((old-extension (file-extension old-path))
-           (old-base-path (file-path old-path))
-           (new-title (% "#fronkensteen-editor-page-title" "val")))
-           (let ((new-path (<< old-base-path "/" (file-basename-no-extension (encode-uri new-title)) "." old-extension)))
-              (if (not (eqv? old-path new-path))
-                (begin
-                  (file-rename old-path new-path)
-                  (let ((old-info (hashtable-ref editor-info-hash id #f)))
-                    (hashtable-set! editor-info-hash id (cons id (cons new-title (cons new-path (cdddr old-info))))))))
-  (write-internal-text-file new-path (cm-editor-get-text id))))))
+  (let ((old-path (get-generic-editor-resource-path id))
+        (title (% "#fronkensteen-editor-page-title" "val")))
+      (let ((new-path (if (is-wiki-path? old-path)
+                          (<< "user-files/wiki/" (encode-uri title) ".fmk")
+                          title)))
+          (if (not (eqv? old-path new-path))
+            (begin
+              (file-rename old-path new-path)
+              (let ((old-info (hashtable-ref editor-info-hash id #f)))
+                (hashtable-set! editor-info-hash id (cons id (cons title (cons new-path (cdddr old-info))))))))
+            (write-internal-text-file new-path (cm-editor-get-text id)))))
 
 (define (generic-editor-file-close id)
   #t ; placeholder
